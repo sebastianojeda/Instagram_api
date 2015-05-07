7e6c1995499a4aefe8753c408c9508aab765ee1f
sebastianojeda <sebastianojeda.school@gmail.com>
2015-05-07 12:03:42 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//Configuration for our php Server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//Make Constants using define
+define('client_id', '6adf1fecc34b4ac4847a5085fc40cf19');
+define('client_Secret', 'f927f361c731457f97e3cc97e4e25b37');
+define('redirectURI', 'http://localhost/LearningAPI/index.php');
+define('ImageDirectory ', 'pics/');
+
+?>
+
+<!-- define('client_id', '6adf1fecc34b4ac4847a5085fc40cf19')
+define('client_Secret', 'f927f361c731457f97e3cc97e4e25b37')
+define('redirectURI', 'http://localhost/LearningAPI/index.php')
+define('ImageDirectory ', 'pics/') -->
+
+
