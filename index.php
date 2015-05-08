a922b6a5b3574ae23fc2476188060438e6e55953
sebastianojeda <sebastianojeda.school@gmail.com>
2015-05-08 12:01:36 -0700

    finished milestone 4 fixed some errors

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define
-define('client_ID', '6adf1fecc34b4ac4847a5085fc40cf19');
-define('client_Secret', 'f927f361c731457f97e3cc97e4e25b37');
+define('clientID', '2ed2f28ebcd2490582a39012eaa4fa4c');
+define('clientSecret', 'f6c872ea0bdf4e36b05fb9287b6b8f7d');
 define('redirectURI', 'http://localhost/LearningAPI/index.php');
 define('ImageDirectory ', 'pics/');
 
@@ -26,7 +26,8 @@ define('ImageDirectory ', 'pics/');
 	<!-- Creating a login for people to go and give approval for our web app to access their instagram account
 			After getting approval we are going to have the info so that we can play with it
 	-->
-		<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID;?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; 
+		?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
 		<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
